ecf46ccc036c1e7fbdf3841a3833944d560ef318
ozgur-catal <168709108+ozgur-catal@users.noreply.github.com>
2024-10-19 10:26:42 +0300

    Sprınt 17 Gün 2 Gün Projesi Bitti

--- src/main/java/com/workintech/s17d2/S17d2Application.java
+++ src/main/java/com/workintech/s17d2/S17d2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class S17d2Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(S17d2Application.class, args);
 	}
 
